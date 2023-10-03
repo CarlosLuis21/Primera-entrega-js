885152cc710fd3e403577a010b99a8f18f234962
carlosluis21 <carlosming21@gmail.com>
2023-10-03 08:16:04 -0300

    Subiendo

---
+++ main.js
@@ -0,0 +1,68 @@
+let opcion1 = "Gold" 
+let opcion2 = "Golf"
+let opcion3 = "Polo"
+
+
+let nombre = prompt("ingresa tu nombre")
+
+alert("Tu nuevo WOLKSWAGEN auto te espera "+ nombre);
+
+seleccion = prompt("Escibre que auto prefieres: Gold, Golf, Polo (recuerda respetar las mayusculas)")    
+
+switch(seleccion) {
+    
+    case "Gold":
+    alert("Excelente elección, precio de lista es de $8.000.000");
+    break;
+
+    case "Golf":
+    alert("Excelente elección, precio de lista es de $12.000.000");
+    break;
+
+    case "Polo":
+    alert("Excelente elección, precio de lista es de $15.000.000");
+    break; 
+    
+    default:
+    alert("Intenta de nuevo "+nombre);
+    prompt("Escibre que auto prefieres: Gold, Golf, Polo (recuerda respetar las mayusculas)")
+    break;
+}
+
+let cuotas = prompt("Escribe la cantidad que cuotas que prefieres, 1, 12 o 24")
+if (seleccion = seleccion)
+
+if (cuotas === 1){
+    alert("Precio final es $8.000.000")
+
+}
+if (cuotas === 12){
+    alert("Precio final es $12.000.000")
+}
+if (cuotas === "24"){
+    alert("Precio final es $14.400.000")
+}
+
+if (seleccion = seleccion)
+
+if (cuotas === "1"){
+    alert("Precio final es $12.000.000")
+}
+if (cuotas === "12"){
+    alert("Precio final es $18.000.000")
+}
+if (cuotas = "24"){
+    alert("Precio final es $21.600.000")
+}
+if (seleccion = seleccion)
+
+if (cuotas === "1"){
+    alert("Precio final es $15.000.000")
+}
+if (cuotas === "12"){
+    alert("Precio final es $22.500.000")
+}
+if (cuotas === "24"){
+    alert("Precio final es $27.000.000")
+}
+ 
