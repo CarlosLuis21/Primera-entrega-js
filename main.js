388bf34fee7b3e433c094558f8f5cf8e40b5342e
carlosluis21 <carlosming21@gmail.com>
2023-10-19 09:15:00 -0300

    segunda entrega

--- main.js
+++ main.js
@@ -1,68 +1,107 @@
+
 let opcion1 = "Gold" 
 let opcion2 = "Golf"
 let opcion3 = "Polo"
 
+let nombre = prompt("Ingresa tu nombre");
 
-let nombre = prompt("ingresa tu nombre")
+if (nombre == null || nombre == "") {
+    do {
+        nombre = prompt
+prompt("Por favor, ingresa tu nombre");
+    } while (nombre === null || nombre == "");
+}
 
-alert("Tu nuevo WOLKSWAGEN auto te espera "+ nombre);
+alert("Tu nuevo VOLKSWAGEN te espera, " + nombre);
 
-seleccion = prompt("Escibre que auto prefieres: Gold, Golf, Polo (recuerda respetar las mayusculas)")    
+let seleccion = prompt("Escribe qué auto prefieres: Gold, Golf, Polo (recuerda respetar las mayúsculas)");
 
-switch(seleccion) {
+switch (seleccion) {
     
-    case "Gold":
-    alert("Excelente elección, precio de lista es de $8.000.000");
-    break;
+   
+case "Gold":
+        alert("Excelente elección, precio de lista es de $8.000.000");
+        break;
 
     case "Golf":
-    alert("Excelente elección, precio de lista es de $12.000.000");
-    break;
+        
+        alert
+alert("Excelente elección, precio de lista es de $12.000.000");
+        break;
 
     case "Polo":
-    alert("Excelente elección, precio de lista es de $15.000.000");
-    break; 
+        alert("Excelente elección, precio de lista es de $15.000.000");
+        break;
+
     
-    default:
-    alert("Intenta de nuevo "+nombre);
-    prompt("Escibre que auto prefieres: Gold, Golf, Polo (recuerda respetar las mayusculas)")
-    break;
+
+   
+default:
+        
+        alert
+alert("Intenta de nuevo, " + nombre);
+        
+       
+break;
 }
 
-let cuotas = prompt("Escribe la cantidad que cuotas que prefieres, 1, 12 o 24")
-if (seleccion = seleccion)
+let cuotas = prompt("Escribe la cantidad de cuotas que prefieres: 1, 12 o 24");
 
-if (cuotas === 1){
-    alert("Precio final es $8.000.000")
+if (cuotas === "1") {
+    
+    
+   
+switch (seleccion) {
+        case "Gold":
+            
+           
+alert("Precio final es $8.000.000");
+            break;
 
-}
-if (cuotas === 12){
-    alert("Precio final es $12.000.000")
-}
-if (cuotas === "24"){
-    alert("Precio final es $14.400.000")
-}
+        case "Golf":
+            
+            alert
+alert("Precio final es $12.000.000");
+            break;
 
-if (seleccion = seleccion)
+        case "Polo":
+            alert("Precio final es $15.000.000");
+            break;
+    }
+} 
+    
+else if (cuotas === "12") {
+    switch (seleccion) {
+        case "Gold":
+            alert("Precio final es $12.000.000");
+            break;
 
-if (cuotas === "1"){
-    alert("Precio final es $12.000.000")
-}
-if (cuotas === "12"){
-    alert("Precio final es $18.000.000")
-}
-if (cuotas = "24"){
-    alert("Precio final es $21.600.000")
-}
-if (seleccion = seleccion)
+        case "Golf":
+            alert("Precio final es $18.000.000");
+            break;
 
-if (cuotas === "1"){
-    alert("Precio final es $15.000.000")
+        case "Polo":
+            
+           
+alert("Precio final es $22.500.000");
+            break;
+    }
+} else if (cuotas === "24") {
+    switch (seleccion) {
+        case "Gold":
+            alert("Precio final es $14.400.000");
+            break;
+
+        case "Golf":
+            
+           
+alert("Precio final es $21.600.000");
+            break;
+
+        case "Polo":
+            alert("Precio final es $27.000.000");
+            break;
+    }
+} else {
+    alert("Selección de cuotas no válida");
 }
-if (cuotas === "12"){
-    alert("Precio final es $22.500.000")
-}
-if (cuotas === "24"){
-    alert("Precio final es $27.000.000")
-}
- 
